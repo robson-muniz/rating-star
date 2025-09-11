d7bdc8dfe0fba16ed032fa6bd69fa87ad27e8295
robson-muniz <robson_muniz@yahoo.com>
2025-09-11 09:06:57 +0100

    List using .map

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,7 +1,17 @@
-const Rating = () =>{
-    return (<div>
-        Rate your experience
-    </div>)
-}
+const Rating = () => {
+    const stars = Array.from({length: 5}, (_, i) => i + 1);
+
+    return (
+        <div className="rating-container">
+            <h2>Rate your experience</h2>
+            <div className="stars">
+                {stars.map((star) => (
+                    <span className= 'star' key={star}>{'\u2605'}</span>
+                ))}
+            </div>
+        </div>
+    );
+};
+
 
 export default Rating;
