2620249164ced21f5eb3b64a1af2b40876e86147
robson-muniz <robson_muniz@yahoo.com>
2025-09-12 14:05:17 +0100

    Handling Events

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,12 +1,21 @@
 const Rating = () => {
     const stars = Array.from({length: 5}, (_, i) => i + 1);
 
+    const clicked = (index) => console.log('clicked', index);
+    const hovered = (direction, index) => console.log('hovered', direction, index);
+
     return (
         <div className="rating-container">
             <h2>Rate your experience</h2>
             <div className="stars">
-                {stars.map((star) => (
-                    <span className= 'star' key={star}>{'\u2605'}</span>
+                {stars.map((star, index) => (
+                    <span
+                        onClick={() =>clicked(index)}
+                        onMouseEnter={() => hovered ('enter', index)}
+                        onMouseLeave={() => hovered ('leave', index)}
+                        className= 'star'
+                        key={star}>
+                        {'\u2605'}</span>
                 ))}
             </div>
         </div>
