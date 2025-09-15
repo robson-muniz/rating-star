9ac4e8633fd0a0ca6d3f84feb46762488db23361
robson-muniz <robson_muniz@yahoo.com>
2025-09-15 15:41:53 +0100

    Props

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,12 @@ import Rating from "./components/Rating.jsx";
 const App = () => {
     return (
         <div>
-            <Rating />
+            <Rating heading='How do you feel about React'
+                    color='red'
+                    feedbackMessages = {[
+                        'Hate it', 'Dislike it', 'Meh', 'Like it', 'Love it'
+                    ]}
+            />
         </div>
 
     );
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,22 +1,25 @@
 import {useState} from "react";
 
-const Rating = () => {
+const Rating = ({heading, color, feedbackMessages}) => {
     const [rating, setRating] = useState(0)
    const [hover, setHover ] = useState(0)
     
     const stars = Array.from({length: 5}, (_, i) => i + 1);
-    const feedbackMessages = ['Terrible', 'Poor', 'Fair', 'Good', 'excelent']
+    // const feedbackMessages = ['Terrible', 'Poor', 'Fair', 'Good', 'excellent']
 
     return (
         <div className="rating-container">
-            <h2>Rate your experience</h2>
+            <h2>{heading}</h2>
             <div className="stars">
                 {stars.map((star) => (
                     <span
                         onClick={() => setRating(star)}
                         onMouseEnter={() => setHover(star)}
                         onMouseLeave={() => setHover(0)}
-                        className= {`star ${star <=(hover || rating) ? 'active' : ''}`}
+                        className= {`star`}
+                        style={{
+                            color: star <= (hover || rating) ? color : '#ccc'
+                        }}
                         key={star}>
                         {'\u2605'}
                     </span>
