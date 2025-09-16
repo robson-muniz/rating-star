ffd2d700f5d1deab537a50fb11f9ab61a768b9b2
robson-muniz <robson_muniz@yahoo.com>
2025-09-16 13:00:24 +0100

    Component Composition

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,4 +1,5 @@
 import {useState} from "react";
+import Star from "./Star.jsx";
 
 const Rating = ({heading, color, feedbackMessages}) => {
     const [rating, setRating] = useState(0)
@@ -12,17 +13,27 @@ const Rating = ({heading, color, feedbackMessages}) => {
             <h2>{heading}</h2>
             <div className="stars">
                 {stars.map((star) => (
-                    <span
-                        onClick={() => setRating(star)}
-                        onMouseEnter={() => setHover(star)}
-                        onMouseLeave={() => setHover(0)}
-                        className= {`star`}
-                        style={{
-                            color: star <= (hover || rating) ? color : '#ccc'
-                        }}
-                        key={star}>
-                        {'\u2605'}
-                    </span>
+                    <Star
+                        key={star}
+                        star={star}
+                        rating={rating}
+                        hover={hover}
+                        color={color}
+                        ratingClick={setRating}
+                        hoverEnter={setHover}
+                        hoverLeave={() => setHover(null)}
+                    />
+                    // <span
+                    //     onClick={() => setRating(star)}
+                    //     onMouseEnter={() => setHover(star)}
+                    //     onMouseLeave={() => setHover(0)}
+                    //     className= {`star`}
+                    //     style={{
+                    //         color: star <= (hover || rating) ? color : '#ccc'
+                    //     }}
+                    //     key={star}>
+                    //     {'\u2605'}
+                    // </span>
                 ))}
             </div>
             {rating > 0 && <p className='feedback'>{feedbackMessages[rating - 1]}</p>}
---
+++ src/components/Star.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Star({star, rating, hover, color, ratingClick, hoverEnter,hoverLeave}) {
+    return (
+       <span
+           onClick={() => ratingClick(star)}
+           onMouseEnter={() => hoverEnter(star)}
+           onMouseLeave={hoverLeave}
+           className='star'
+           style={{color: star <= (hover || rating) ? color : '#ccc'}}
+
+       >
+
+           {'\u2605'}</span>
+    );
+}
+
+export default Star;
